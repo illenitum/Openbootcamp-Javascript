c28c58a2823fae6f2f7af2187377ba8f88feb5ee
illenitum <107963232+illenitum@users.noreply.github.com>
2022-12-01 19:57:21 -0300

    Add files via upload

---
+++ Ejercicio-02-sintaxis/Ejercicio-02.js
@@ -0,0 +1,13 @@
+
+
+const name  ='Humberto';
+const age = 50;
+const isDeveloper = true;
+const birthDate = new Date(11, 3, 1972);
+const favoriteBook = {
+    title: 'Start wiht why',
+    author: 'Simon Sinek',
+    Year: 2009,
+    url: 'https://simonsinek.com/books/start-with-why/'
+
+}
