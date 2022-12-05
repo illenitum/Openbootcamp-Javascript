a64340cf4b25bb517a44f4662e861121f2ad6642
illenitum <107963232+illenitum@users.noreply.github.com>
2022-12-05 17:02:32 -0300

    Add files via upload

---
+++ Ejercicio-05-numbers-js/numbers.js
@@ -0,0 +1,23 @@
+// My height in centimeters
+let height_cm = 174
+
+// My height in meters
+let height_mt = height_cm / 100
+
+// My weight in kilos
+let weight_kg = 82.5
+
+//My height in meters rounded up
+let height_mt_round = Math.ceil(height_mt)
+
+// My weight in kg rounded down
+let weight_kg_round = Math.floor(weight_kg)
+
+
+// Maximum value in Javascript + 1 == Maximum Value in Javascript
+let max_value_JS = Number.MAX_VALUE +1
+is_equal = (max_value_JS == Number.MAX_VALUE)
+console.log(is_equal)
+
+
+
